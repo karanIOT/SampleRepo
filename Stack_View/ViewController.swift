3f6916c4b69773b019b3b6d46586da8fe8c0a095
Rathore, Karanvir (US - Mumbai) <karrathore@deloitte.com>
2016-08-30 17:59:41 +0530

    Initial Commit

---
+++ Stack_View/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Stack_View
+//
+//  Created by Rathore, Karanvir (US - Mumbai) on 1/26/16.
+//  Copyright © 2016 Rathore, Karanvir (US - Mumbai). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var axisSwitch: UISwitch!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
+    @IBOutlet weak var axisLabel: UILabel!
+
+    @IBAction func axisChange(sender: UISwitch) {
+        UIView.animateWithDuration(0.9) {
+            self.updateConstraintsForAxis();
+        }
+    }
+    
+    private func updateConstraintsForAxis()
+    {
+        if(axisSwitch.on)
+        {
+           axisLabel.text = "Horizontal"
+           stackView.axis = .Horizontal
+        }
+        else
+        {
+            axisLabel.text = "Vertical"
+            stackView.axis = .Vertical
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.updateConstraintsForAxis();
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
